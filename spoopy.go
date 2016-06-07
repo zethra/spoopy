da8cf54aed70db7521d88536a3eaa61e11f245d9
zethra <jediben97@gmail.com>
2016-06-06 23:54:50 -0400

    Spoopilishus

--- spoopy.go
+++ spoopy.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"net/http"
+	"log"
+	"io"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/download", downloadHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func downloadHandler(writer http.ResponseWriter, request *http.Request)  {
+	err := request.ParseForm()
+	if err != nil {
+		http.Error(writer, "Bad request", 400)
+		log.Fatal(err)
+	}
+	url := request.Form.Get("url")
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(writer, "Could not retrive file", 500)
+		log.Fatal(err)
+	}
+	writer.Header().Set("Content-Disposition", resp.Header.Get("Content-Disposition") + ".html")
+	writer.Header().Set("Content-Type", "text/html")
+	io.Copy(writer, resp.Body)
+}
